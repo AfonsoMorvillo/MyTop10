463499d6944f2df8948a83fa7296cca4e991a15a
afonsomorvillo <afonsomorvillo1@gmail.com>
2024-08-08 11:16:28 -0300

    Iniciando projeto

---
+++ src/main/java/edu/ifsp/web/CadastroTop10Servlet.java
@@ -0,0 +1,19 @@
+package edu.ifsp.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import edu.ifsp.web.templates.Template;
+
+@WebServlet("/cadastro")
+public class CadastroTop10Servlet extends HttpServlet {
+
+   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {      
+      Template.render("top10/novo", request, response);      
+   }
+}
