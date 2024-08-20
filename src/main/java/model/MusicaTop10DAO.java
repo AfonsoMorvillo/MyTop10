5be416cbfdd69bb6937bec38cb8993fd340c56ef
aluno <aluno@cti-OptiPlex-3080>
2024-08-20 11:21:07 -0300

    fase beta do visualizar

--- src/main/java/model/MusicaTop10DAO.java
+++ src/main/java/model/MusicaTop10DAO.java
@@ -1,5 +1,6 @@
 package model;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -26,36 +27,34 @@ public class MusicaTop10DAO {
         }
     }
 	
-//	public List<MusicaTop10> listarTodosMusicaTop10() {
-//        List<MusicaTop10> resultado = new ArrayList<>();
-//        String sql = "SELECT m.ID as id_spotify, m.Descricao AS musica_descricao, m.ordem_musica, t.Titulo AS top10_titulo, t.Descricao AS top10_descricao " +
-//                     "FROM musica_top10 mt " +
-//                     "LEFT JOIN musica m ON mt.ID_musica = m.ID " +
-//                     "LEFT JOIN top10 t ON mt.ID_top10 = t.ID";
-//
-//        try (Connection conn = DatabaseConnection.getInstance().getConn();
-//             PreparedStatement ps = conn.prepareStatement(sql);
-//             ResultSet rs = ps.executeQuery()) {
-//
-//            while (rs.next()) {
-//                MusicaTop10 item = new MusicaTop10();
-////                item.set(rs.getString("musica_descricao"));
-////                item.setOrdem(rs.getInt("ordem_musica"));
-////                item.setTop10Titulo(rs.getString("top10_titulo"));
-////                item.setTop10Descricao(rs.getString("top10_descricao"));
-//                item.getTop10().setTitulo(rs.getString("top10_titulo"));
-//                item.getTop10().setDescricao(rs.getString("top10_descricao"));
-//                item.getMusica().setId(rs.getString("id_spotify"));
-//                item.getMusica().setOrdem(rs.getInt("ordem_musica"));
-//                resultado.add(item);
-//            }
-//
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//
-//        return resultado;
-//    }
+	public List<MusicaTop10> listarTodasMusicaTop10() {
+		 DatabaseConnection instance = DatabaseConnection.getInstance();
+
+        List<MusicaTop10> resultado = new ArrayList<>();
+        String sql = "SELECT m.ID as id_spotify, m.Descricao AS musica_descricao, m.ordem_musica, t.Titulo AS top10_titulo, t.Descricao AS top10_descricao " +
+                     "FROM musica_top10 mt " +
+                     "LEFT JOIN musica m ON mt.ID_musica = m.ID " +
+                     "LEFT JOIN top10 t ON mt.ID_top10 = t.ID";
+
+        try (
+             PreparedStatement ps = instance.prepareStatement(sql);
+             ResultSet rs = ps.executeQuery()) {
+
+            while (rs.next()) {
+                MusicaTop10 item = new MusicaTop10();
+                item.getTop10().setTitulo(rs.getString("top10_titulo"));
+                item.getTop10().setDescricao(rs.getString("top10_descricao"));
+                item.getMusica().setId(rs.getString("id_spotify"));
+                item.getMusica().setOrdem(rs.getInt("ordem_musica"));
+                resultado.add(item);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return resultado;
+    }
 
     
     public List<MusicaTop10> listarMusicaTop10PorTitulo(Integer chave) {
